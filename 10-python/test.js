05d48f329a0c44717eef12cde7e4e0ddd47ca897
Jason <251222824@qq.com>
2021-10-03 11:35:42 +0800

    update

---
+++ 10-python/test.js
@@ -0,0 +1,15 @@
+var JSEncrypt = require("./jsencrypt.js");
+
+// function jsencrypt(publicKey, pwd) {
+//   const encrypt = new JSEncrypt();
+//   console.log(encrypt);
+//   encrypt.setPublicKey(publicKey);
+//   const encrypted = encrypt.encrypt(pwd);
+//   return encrypted;
+// }
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(1, 2));
